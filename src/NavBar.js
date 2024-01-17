048a0b7a4eacd047f8e88a7c75bd6467fe2ebfd0
belissa28 <41009713+belissa28@users.noreply.github.com>
2024-01-17 02:15:15 -0500

    Using material card and added image in home page.

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,27 +7,27 @@ const NavBar = () => {
     return (
         <Navbar bg="black" expand="lg" className={styles.navbar}>
             <Container className={styles.navbarContainer}>
-                <Navbar.Brand as={NavLink} to="/" style={{ color: 'white' }} >Belissa</Navbar.Brand>
+                <Navbar.Brand as={NavLink} to="/" style={{ color: 'white', fontWeight: 'bold' }} >Belissa Baez</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" >
-                        <Nav.Link as={NavLink} to="/" className={styles.navLink} style={{ color: 'white' }} >
+                        <Nav.Link as={NavLink} to="/" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
                             Home
             </Nav.Link>
-                        <Nav.Link as={NavLink} to="/about" className={styles.navLink} style={{ color: 'white' }} >
+                        <Nav.Link as={NavLink} to="/about" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
                             About
             </Nav.Link>
-                        <Nav.Link as={NavLink} to="/work" className={styles.navLink} style={{ color: 'white' }} >
+                        <Nav.Link as={NavLink} to="/work" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
                             Work
             </Nav.Link>
 
-                        <Nav.Link as={NavLink} to="/contact" className={styles.navLink} style={{ color: 'white' }} >
+                        <Nav.Link as={NavLink} to="/contact" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
                             Contact
             </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,62 @@
 import React from 'react';
-
-
+import Belissacomputer from './Belissacomputer.jpg';
+import Box from '@mui/material/Box';
+import CardMedia from '@mui/material/CardMedia';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// const bull = (
+//     <Box
+//         component="span"
+//         sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+//     >
+//         •
+//     </Box>
+// );
 function Home() {
     return (
         <div>
-            <h1>Home Page bb</h1>
+
+            {/* <div>
+                <img src={Belissacomputer} alt="Belissa" height={150} width={250} />
+                <h1> Home Page bb</h1>
+            </div> */}
+
+            <div style={{ margin: '7%', height: '20px' }}>
+                <Card sx={{ maxWidth: 12000 }}>
+                    <CardContent>
+                        {/* <Typography variant='h3' gutterBottom>
+                            A Simple Material UI Card
+            </Typography> */}
+                        <CardMedia
+                            component="img"
+                            height="650"
+                            width="140"
+                            image={Belissacomputer}
+                            alt="Belissa"
+                        />
+                        {/* <Typography variant="h4" component="div">
+                            Heading
+            </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            describes the heading
+            </Typography>
+                        <Typography variant="body1">
+                            Card content
+              <br />
+                            {'"describes the content"'}
+                        </Typography> */}
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">About me</Button>
+                    </CardActions>
+                </Card>
+            </div>
         </div>
-
-
     );
+
 };
 
 // const Home = () => <h1>Home Page</h1>;
