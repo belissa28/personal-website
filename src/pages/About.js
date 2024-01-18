d46450ba2927cf6d41081023009ce18fdf90bed5
belissa28 <41009713+belissa28@users.noreply.github.com>
2024-01-18 01:40:24 -0500

    Added image and text to the about page.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,24 @@
 // About.js
 import React from 'react';
+import Belissaheart from './Belissaheart.jpg';
+import './styles.css';
+
+function About() {
+    return (
+        // <div>
+        //     <img src={Belissaheart} alt="belissa" height={500} width={400} />
+        //     <p >This is some text to the right of the image.</p>
+        // </div>
+        <div className="about-container">
+            <img src={Belissaheart} alt="heart" height={500} width={400} />
+            <p>This is some text to the right of the image.</p>
+        </div >
+
+    );
+
+}
 
 
-const About = () => <h1>About Page</h1>;
+// const About = () => <h1>About Page</h1>;
 
 export default About;
