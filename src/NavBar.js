06cf1f79ec4d68729bb840692d68b4c199f57c4e
belissa28 <41009713+belissa28@users.noreply.github.com>
2024-04-08 23:43:25 -0400

    Pushed navlinks to the right of navbar.

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,33 +5,23 @@ import styles from './NavBar.module.css';
 
 const NavBar = () => {
     return (
-        <Navbar bg="black" expand="lg" className={styles.navbar}>
-            <Container className={styles.navbarContainer}>
-                <Navbar.Brand as={NavLink} to="/" style={{ color: 'white', fontWeight: 'bold' }} >Belissa Baez</Navbar.Brand>
+        <Navbar bg="black" expand="lg">
+            <Container fluid className={styles.navbarContainer}>
+                <Navbar.Brand as={NavLink} to="/" className={styles.navbar} style={{ color: 'white', fontWeight: 'bold', marginLeft: 'auto' }}>React App</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto" >
-                        <Nav.Link as={NavLink} to="/" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
-                            Home
-            </Nav.Link>
-                        <Nav.Link as={NavLink} to="/about" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
-                            About
-            </Nav.Link>
-                        <Nav.Link as={NavLink} to="/work" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
-                            Work
-            </Nav.Link>
-                        <Nav.Link as={NavLink} to="/funfacts" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
-                            Fun Facts
-            </Nav.Link>
-
-
-                        <Nav.Link as={NavLink} to="/contact" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }} >
-                            Contact
-            </Nav.Link>
+                    <Nav className="ms-auto" >
+                        <Nav.Link as={NavLink} to="/" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>About</Nav.Link>
+                        <Nav.Link as={NavLink} to="/work" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>Work</Nav.Link>
+                        <Nav.Link as={NavLink} to="/funfacts" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>Fun Facts</Nav.Link>
+                        <Nav.Link as={NavLink} to="/contact" className={styles.navLink} style={{ color: 'white', fontFamily: 'Arial, sans-serif' }}>Contact</Nav.Link>
                     </Nav>
+
                 </Navbar.Collapse>
             </Container>
-        </Navbar >
+        </Navbar>
+
     );
 };
 
